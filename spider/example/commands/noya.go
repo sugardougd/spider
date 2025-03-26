4303ee2c7953287f22f72213a14bdb02b90efb65
leiqian <leiqian@xylink.com>
2025-03-26 17:54:37 +0800

    spider 2025/03/26[1]

--- spider/example/commands/noya.go
+++ spider/example/commands/noya.go
@@ -43,14 +43,51 @@ func NoyaCommand() *spider.Command {
 				return err
 			}
 			out := make(chan string)
-			go calculate(count, []mathOperator{&mathAddOperator{low, top}}, out)
+			go calculate(count, []mathOperator{
+				&mathAddOperator{mathParams{low: low, top: top}},
+			}, out)
 			for o := range out {
 				c.Spider.Println(o)
 			}
 			return nil
 		},
 	}
-	// aub
+
+	// keep-add
+	addKeep := &spider.Command{
+		Name:        "math-add-keep",
+		Description: "displays math-add-keep problem",
+		Usage:       "noya math-add-keep",
+		Flags: func(f *spider.Flags) {
+			f.Int(&spider.Flag{Short: "l", Long: "low", Help: "the low value", Default: 10})
+			f.Int(&spider.Flag{Short: "t", Long: "top", Help: "the top value", Default: 10})
+			f.Int(&spider.Flag{Short: "c", Long: "count", Help: "the count of problem", Default: 10})
+		},
+		Run: func(c *spider.Context) error {
+			low, err := c.FlagValues.Int(c.Command, "low")
+			if err != nil {
+				return err
+			}
+			top, err := c.FlagValues.Int(c.Command, "top")
+			if err != nil {
+				return err
+			}
+			count, err := c.FlagValues.Int(c.Command, "count")
+			if err != nil {
+				return err
+			}
+			out := make(chan string)
+			go calculate(count, []mathOperator{
+				&mathAddKeepOperator{mathParams{low: low, top: top}},
+			}, out)
+			for o := range out {
+				c.Spider.Println(o)
+			}
+			return nil
+		},
+	}
+
+	// sub
 	sub := &spider.Command{
 		Name:        "math-sub",
 		Description: "displays math-sub problem",
@@ -74,13 +111,50 @@ func NoyaCommand() *spider.Command {
 				return err
 			}
 			out := make(chan string)
-			go calculate(count, []mathOperator{&mathSubOperator{low, top}}, out)
+			go calculate(count, []mathOperator{
+				&mathSubOperator{mathParams{low: low, top: top}},
+			}, out)
 			for o := range out {
 				c.Spider.Println(o)
 			}
 			return nil
 		},
 	}
+
+	// sub
+	subKeep := &spider.Command{
+		Name:        "math-sub-keep",
+		Description: "displays math-sub-keep problem",
+		Usage:       "noya math-sub-keep",
+		Flags: func(f *spider.Flags) {
+			f.Int(&spider.Flag{Short: "l", Long: "low", Help: "the low value", Default: 10})
+			f.Int(&spider.Flag{Short: "t", Long: "top", Help: "the top value", Default: 10})
+			f.Int(&spider.Flag{Short: "c", Long: "count", Help: "the count of problem", Default: 10})
+		},
+		Run: func(c *spider.Context) error {
+			low, err := c.FlagValues.Int(c.Command, "low")
+			if err != nil {
+				return err
+			}
+			top, err := c.FlagValues.Int(c.Command, "top")
+			if err != nil {
+				return err
+			}
+			count, err := c.FlagValues.Int(c.Command, "count")
+			if err != nil {
+				return err
+			}
+			out := make(chan string)
+			go calculate(count, []mathOperator{
+				&mathSubKeepOperator{mathParams{low: low, top: top}},
+			}, out)
+			for o := range out {
+				c.Spider.Println(o)
+			}
+			return nil
+		},
+	}
+
 	// add & sub
 	addSub := &spider.Command{
 		Name:        "math-add-sub",
@@ -105,16 +179,61 @@ func NoyaCommand() *spider.Command {
 				return err
 			}
 			out := make(chan string)
-			go calculate(count, []mathOperator{&mathAddOperator{low, top}, &mathSubOperator{low, top}}, out)
+			go calculate(count, []mathOperator{
+				&mathAddOperator{mathParams{low: low, top: top}},
+				&mathSubOperator{mathParams{low: low, top: top}},
+			},
+				out)
 			for o := range out {
 				c.Spider.Println(o)
 			}
 			return nil
 		},
 	}
+
+	// add & sub
+	addSubKeep := &spider.Command{
+		Name:        "math-add-sub-keep",
+		Description: "displays math-addkeep & math-subkeep problem",
+		Usage:       "noya math-add-sub-keep",
+		Flags: func(f *spider.Flags) {
+			f.Int(&spider.Flag{Short: "l", Long: "low", Help: "the low value", Default: 10})
+			f.Int(&spider.Flag{Short: "t", Long: "top", Help: "the top value", Default: 10})
+			f.Int(&spider.Flag{Short: "c", Long: "count", Help: "the count of problem", Default: 10})
+		},
+		Run: func(c *spider.Context) error {
+			low, err := c.FlagValues.Int(c.Command, "low")
+			if err != nil {
+				return err
+			}
+			top, err := c.FlagValues.Int(c.Command, "top")
+			if err != nil {
+				return err
+			}
+			count, err := c.FlagValues.Int(c.Command, "count")
+			if err != nil {
+				return err
+			}
+			out := make(chan string)
+			go calculate(count, []mathOperator{
+				&mathAddKeepOperator{mathParams{low: low, top: top}},
+				&mathSubKeepOperator{mathParams{low: low, top: top}},
+				&mathAddSubKeepOperator{mathParams{low: low, top: top}},
+			},
+				out)
+			for o := range out {
+				c.Spider.Println(o)
+			}
+			return nil
+		},
+	}
+
 	noya.AddCommand(add)
+	noya.AddCommand(addKeep)
 	noya.AddCommand(sub)
+	noya.AddCommand(subKeep)
 	noya.AddCommand(addSub)
+	noya.AddCommand(addSubKeep)
 	return noya
 }
 
@@ -131,25 +250,73 @@ type mathOperator interface {
 	calculate() (string, error)
 }
 
-type mathAddOperator struct {
+type mathParams struct {
 	low int
 	top int
 }
 
+type mathAddOperator struct {
+	mathParams
+}
+
 func (add *mathAddOperator) calculate() (string, error) {
-	num1 := r.Intn(add.top+1-add.low) + add.low
-	num2 := r.Intn(num1 + 1)
+	num1 := r.Intn(add.top+1-add.low) + add.low // [low,top]
+	num2 := r.Intn(num1-1) + 1                  // [1, num1-1]
 	num3 := num1 - num2
 	return fmt.Sprintf("%d + %d = ", num2, num3), nil
 }
 
+type mathAddKeepOperator struct {
+	mathParams
+}
+
+func (add *mathAddKeepOperator) calculate() (string, error) {
+	num1 := r.Intn(add.top+1-add.low) + add.low // [low,top]
+	num2 := r.Intn(num1-3) + 2                  // [2, num1-2]
+	num3 := r.Intn(num1-num2-1) + 1
+	num4 := num1 - num2 - num3
+	return fmt.Sprintf("%d + %d + %d = ", num2, num3, num4), nil
+}
+
 type mathSubOperator struct {
-	low int
-	top int
+	mathParams
 }
 
 func (sub *mathSubOperator) calculate() (string, error) {
-	num1 := r.Intn(sub.top+1-sub.low) + sub.low
-	num2 := r.Intn(num1 + 1)
+	num1 := r.Intn(sub.top+1-sub.low) + sub.low // [low,top]
+	num2 := r.Intn(num1-1) + 1                  // [1, num1-1]
 	return fmt.Sprintf("%d - %d = ", num1, num2), nil
+}
+
+type mathSubKeepOperator struct {
+	mathParams
+}
+
+func (sub *mathSubKeepOperator) calculate() (string, error) {
+	num1 := r.Intn(sub.top+1-sub.low) + sub.low // [low,top]
+	num2 := r.Intn(num1-3) + 2                  // [2, num1-2]
+	num3 := r.Intn(num1-num2-1) + 1
+	return fmt.Sprintf("%d - %d - %d = ", num1, num2, num3), nil
+}
+
+type mathAddSubKeepOperator struct {
+	mathParams
+}
+
+func (o *mathAddSubKeepOperator) calculate() (string, error) {
+	num1 := r.Intn(o.top+1-o.low) + o.low // [low,top]
+	num2 := r.Intn(num1-3) + 2            // [2, num1-2]
+	num3 := r.Intn(num1-num2-1) + 1
+	num4 := num1 - num2 - num3
+	t := r.Intn(4)
+	if t == 0 {
+		return fmt.Sprintf("%d + %d + %d = ", num2, num3, num4), nil
+	}
+	if t == 1 {
+		return fmt.Sprintf("%d + %d - %d = ", num2, num3+num4, num4), nil
+	}
+	if t == 2 {
+		return fmt.Sprintf("%d - %d + %d = ", num2+num3, num3, num4), nil
+	}
+	return fmt.Sprintf("%d - %d - %d = ", num1, num2, num3), nil
 }
